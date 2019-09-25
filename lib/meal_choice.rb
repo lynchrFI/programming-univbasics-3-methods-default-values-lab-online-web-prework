f572f9ed9d2cf45bbbcadd27f0346f199b8dabb5
crisp-unit-4632 <lynchrbusiness@gmail.com>
2019-09-25 06:08:23 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,6 @@
-# Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+  puts meal_choice "What a nutritious meal!"
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}." 
+end
+meal_choice("broccoli", "macaroni")
